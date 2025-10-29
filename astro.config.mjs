1336818f676f390ad4bea183ed2ab55c12269d7d
Angelo Alvarado <alvaradoleon59@gmail.com>
2025-10-29 12:51:58 -0700

    finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx()],
+  vite: {
+    plugins: [tailwind()],
+  },
+});
---
+++ src/config/site-config.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  title: "Angelo's Blog",
+  description: 'Blog oficial de Angelo Alvarado Full Stack',
+};
